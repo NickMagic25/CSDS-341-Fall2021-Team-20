9d8bc9397c55c84298438c70e618bbae8cade93d
Nick <majkic65@gmail.com>
2021-12-02 19:12:19 -0500

    Made all calls of recipe and ingredient names lowercase to ensure no overlap, also changed existing names in database to be all lowercase

--- src/Querys/ExampleQuery.java
+++ src/Querys/ExampleQuery.java
@@ -1,6 +1,7 @@
 package Querys;
 
 import java.sql.*;
+import java.util.Locale;
 import java.util.Random;
 
 public class ExampleQuery {
@@ -68,7 +69,7 @@ public class ExampleQuery {
 	// find nutrition of an ingredient
 	public ResultSet FNOAI(String ingre_Name, String cnnStr) {
 		System.out.println("Looking for nutrition detail for " + ingre_Name);
-		String sql = "Select * From [Nutrition] Where [Nutrition_ID] = (Select [Nutrition_ID] From [Contains] Where [Ingredient_Name] = '" + ingre_Name + "')";
+		String sql = "Select * From [Nutrition] Where [Nutrition_ID] = (Select [Nutrition_ID] From [Contains] Where [Ingredient_Name] = '" + ingre_Name.toLowerCase() + "')";
 		ResultSet resultSet = null;
 		try(Connection cnn = DriverManager.getConnection(cnnStr); Statement statement = cnn.createStatement();){
 			resultSet = statement.executeQuery(sql);
@@ -102,7 +103,7 @@ public class ExampleQuery {
 	// find all ingredients in a recipe
 	public ResultSet FAIIR(String resp_Name, String cnnStr) {
 		System.out.println("Looking for all ingredient needed for " + resp_Name);
-		String sql = "Select * From [Uses] Where [Recipe_Name] = '" + resp_Name +"'";
+		String sql = "Select * From [Uses] Where [Recipe_Name] = '" + resp_Name.toLowerCase() +"'";
 		ResultSet resultSet = null;
 		try(Connection cnn = DriverManager.getConnection(cnnStr); Statement statement = cnn.createStatement();){
 			resultSet = statement.executeQuery(sql);
@@ -119,7 +120,7 @@ public class ExampleQuery {
 	// find all recipes with general taste profile?
 	public ResultSet FARWGTP(String taste_Profile, String cnnStr) {
 		System.out.println("Looking for all recipe with " + taste_Profile);
-		String sql = "Select * From [Taste_Profile] Where [Taste] = '" + taste_Profile +"'";
+		String sql = "Select * From [Taste_Profile] Where [Taste] = '" + taste_Profile.toLowerCase() +"'";
 		ResultSet resultSet = null;
 		try(Connection cnn = DriverManager.getConnection(cnnStr); Statement statement = cnn.createStatement();){
 			resultSet = statement.executeQuery(sql);
@@ -147,7 +148,7 @@ public class ExampleQuery {
 	// find all recipe with general taste profile and region
 	public ResultSet FARWGTP(String taste, String region, String cnnStr) {
 		System.out.println("Looking for all recipe with " + taste + " and " + region);
-		String sql = "Select * From [Taste_Profile] Where [Taste] = '" + taste +"' and [Region] = '" + region + "'";
+		String sql = "Select * From [Taste_Profile] Where [Taste] = '" + taste.toLowerCase() +"' and [Region] = '" + region.toLowerCase() + "'";
 		ResultSet resultSet = null;
 		try(Connection cnn = DriverManager.getConnection(cnnStr); Statement statement = cnn.createStatement();){
 			resultSet = statement.executeQuery(sql);
@@ -164,7 +165,7 @@ public class ExampleQuery {
 	// find nutrition of a recipe
 	public ResultSet FMR(String recipe_name, String cnnStr){
 		System.out.println("Looking for macros of " + recipe_name);
-		String sql="Select n1.Grams_Protien, n1.Grams_Carbs, n1.Grams_Fat From [Nutrition] as n1, [Macro_Contents] as m1 Where (m1.Recipe_Name='"+recipe_name+"') and (n1.Nutrition_ID=m1.Nutrition_ID)";
+		String sql="Select n1.Grams_Protien, n1.Grams_Carbs, n1.Grams_Fat From [Nutrition] as n1, [Macro_Contents] as m1 Where (m1.Recipe_Name='"+recipe_name.toLowerCase()+"') and (n1.Nutrition_ID=m1.Nutrition_ID)";
 		ResultSet macros=null;
 		try(Connection cnn = DriverManager.getConnection(cnnStr); Statement statement = cnn.createStatement();){
 			macros = statement.executeQuery(sql);
@@ -180,8 +181,8 @@ public class ExampleQuery {
 
 	// find all recipes that matches a given ingredient
 	public ResultSet FARI(String ingre_name, String cnnStr){
-		System.out.println("Finding all recipes that use "+ ingre_name );
-		String sql="SELECT u.Recipe_Name, u.Amount_Of FROM [USES] as u  WHERE u.Ingredient_Name='"+ingre_name+"'";
+		System.out.println("Finding all recipes that use "+ ingre_name.toLowerCase() );
+		String sql="SELECT u.Recipe_Name, u.Amount_Of FROM [USES] as u  WHERE u.Ingredient_Name='"+ingre_name.toLowerCase()+"'";
 		ResultSet recipes=null;
 		try(Connection cnn = DriverManager.getConnection(cnnStr); Statement statement = cnn.createStatement();){
 			recipes = statement.executeQuery(sql);
@@ -243,7 +244,7 @@ public class ExampleQuery {
 	 */
 	private boolean FIE(String Name, String cnnStr) {
 		System.out.println("Finding " + Name);
-		String sql= "Select [Name] From [Ingredient] Where [Name] = '" + Name + "'";
+		String sql= "Select [Name] From [Ingredient] Where [Name] = '" + Name.toLowerCase() + "'";
 		ResultSet resultSet = null;
 		try(Connection cnn = DriverManager.getConnection(cnnStr); Statement statement = cnn.createStatement();){
 			resultSet = statement.executeQuery(sql);
@@ -263,7 +264,7 @@ public class ExampleQuery {
 	 */
 	public void AI(String Name, String cnnStr) {
 		System.out.println("Adding ingredient into database");
-		String sql = "Insert into [Ingredient]([Name], [Food_type], [Nutrition_ID]) Values('" + Name+ "', 'unknown', -1)";
+		String sql = "Insert into [Ingredient]([Name], [Food_type], [Nutrition_ID]) Values('" + Name.toLowerCase()+ "', 'unknown', -1)";
 		try(Connection cnn = DriverManager.getConnection(cnnStr); PreparedStatement statement = cnn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
 			ResultSet resultSet = null;
 			statement.execute();
@@ -301,7 +302,7 @@ public class ExampleQuery {
 	 */
 	public void ATP(String Taste, String R_Name, String cnnStr) {
 		System.out.println("Adding Taste_Profile into database");
-		String sql = "Insert into [Taste_Profile]([R_Name], [Taste], [Region], [Dietary_Rest]) Values('" + R_Name+ "', '" + Taste + "', 'Unknown', 'no')";
+		String sql = "Insert into [Taste_Profile]([R_Name], [Taste], [Region], [Dietary_Rest]) Values('" + R_Name.toLowerCase()+ "', '" + Taste.toLowerCase() + "', 'Unknown', 'no')";
 		try(Connection cnn = DriverManager.getConnection(cnnStr); PreparedStatement statement = cnn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
 			ResultSet resultSet = null;
 			statement.execute();
@@ -322,7 +323,7 @@ public class ExampleQuery {
 	 */
 	public void AHT(String Taste, String R_Name, String cnnStr) {
 		System.out.println("Adding Has_Taste into database");
-		String sql = "Insert into [Has_Taste]([Taste], [R_Name], [Name]) Values('" + Taste + "', '" + R_Name + "', '+" + R_Name + "', 'no')";
+		String sql = "Insert into [Has_Taste]([Taste], [R_Name], [Name]) Values('" + Taste.toLowerCase() + "', '" + R_Name.toLowerCase() + "', '+" + R_Name.toLowerCase() + "', 'no')";
 		try(Connection cnn = DriverManager.getConnection(cnnStr); PreparedStatement statement = cnn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
 			ResultSet resultSet = null;
 			statement.execute();
@@ -343,7 +344,7 @@ public class ExampleQuery {
 	 */
 	public void AMC(String R_Name, String N_ID, String cnnStr) {
 		System.out.println("Adding Macro_Contents into database");
-		String sql = "Insert into [Macro_Contents]([Recipe_Name], [Nutrition_ID]) Values('" + R_Name + "', " + N_ID + ")";
+		String sql = "Insert into [Macro_Contents]([Recipe_Name], [Nutrition_ID]) Values('" + R_Name.toLowerCase() + "', " + N_ID + ")";
 		try(Connection cnn = DriverManager.getConnection(cnnStr); PreparedStatement statement = cnn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
 			ResultSet resultSet = null;
 			statement.execute();
@@ -365,7 +366,7 @@ public class ExampleQuery {
 	 */
 	public void AU(String I_Name, String R_Name, String cnnStr) {
 		System.out.println("Adding Uses into database");
-		String sql = "Insert into [Uses]([Ingredient_Name], [Recipee_Name], [Amount_Of]) Values('" + I_Name + "', '" + R_Name + "', 1)";
+		String sql = "Insert into [Uses]([Ingredient_Name], [Recipee_Name], [Amount_Of]) Values('" + I_Name.toLowerCase() + "', '" + R_Name.toLowerCase() + "', 1)";
 		try(Connection cnn = DriverManager.getConnection(cnnStr); PreparedStatement statement = cnn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
 			ResultSet resultSet = null;
 			statement.execute();
@@ -383,7 +384,7 @@ public class ExampleQuery {
 	 * adding recipe
 	 */
 	public void AR(String Name, String instructions, String Diff, String Taste, String Ingredients, String cnnStr){
-		System.out.println("Adding recipe " + Name + "into the database");
+		System.out.println("Adding recipe " + Name.toLowerCase() + "into the database");
 		String[] Ingredient = Ingredients.split(",");
 		Boolean[] exist = new Boolean[Ingredient.length];
 		for(Boolean e : exist)
@@ -400,7 +401,7 @@ public class ExampleQuery {
 			ATP(Taste, Name, cnnStr);
 		}
 		String sql = "Insert into [Recipe]([Name], [Instructions], [Diffculty_Level], [Nutrition_ID], [Taste_Profile]) "
-				+ "Values('"+ Name +"', '" + instructions + "', " + Diff + ", -1, '" + Taste + "')";
+				+ "Values('"+ Name.toLowerCase() +"', '" + instructions + "', " + Diff + ", -1, '" + Taste + "')";
 		try(Connection cnn = DriverManager.getConnection(cnnStr);
 			PreparedStatement statement = cnn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
 				ResultSet resultSet = null;
@@ -500,7 +501,7 @@ public class ExampleQuery {
 
 	public void ate(String user_id, String r_name, String cnnStr){
 		System.out.println("User " + user_id + " ate " + r_name);
-		String sql="Insert into [Ate](User_ID, Recipe_Name, Nutrition_ID) Values(" + user_id + "," + r_name + ", -1)";
+		String sql="Insert into [Ate](User_ID, Recipe_Name, Nutrition_ID) Values(" + user_id + "," + r_name.toLowerCase() + ", -1)";
 		try(Connection cnn = DriverManager.getConnection(cnnStr); PreparedStatement statement = cnn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
 			ResultSet resultSet = null;
 			statement.execute();
@@ -512,7 +513,7 @@ public class ExampleQuery {
 	}
 
 	public boolean recipeExists(String r_name, String cnnStr){
-		String sql= "Select Name FROM [Recipe] WHERE Name='" + r_name + "'";
+		String sql= "Select Name FROM [Recipe] WHERE Name='" + r_name.toLowerCase() + "'";
 		ResultSet resultSet = null;
 		try(Connection cnn = DriverManager.getConnection(cnnStr); Statement statement = cnn.createStatement();){
 			resultSet = statement.executeQuery(sql);
@@ -549,7 +550,7 @@ public class ExampleQuery {
 			addIngredient(user_ID,ingre_name,amount,cnnStr);
 		else
 		{
-			String sql="Update [Has] Set Serving_Size=Serving_Size+"+ amount + "WHERE User_ID="+ user_ID + "and Name='"+ ingre_name +"'";
+			String sql="Update [Has] Set Serving_Size=Serving_Size+"+ amount + "WHERE User_ID="+ user_ID + "and Name='"+ ingre_name.toLowerCase() +"'";
 			try(Connection cnn = DriverManager.getConnection(cnnStr); PreparedStatement statement = cnn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
 				ResultSet resultSet = null;
 				statement.execute();
@@ -562,7 +563,7 @@ public class ExampleQuery {
 	}
 
 	private void addIngredient(String user_ID, String ingre_name,String amount ,String cnnStr){
-		String sql="Insert [Has](User_ID, Name, Serving_Size) Values("+ user_ID + ",'" + ingre_name + "'," + amount + ")";
+		String sql="Insert [Has](User_ID, Name, Serving_Size) Values("+ user_ID + ",'" + ingre_name.toLowerCase() + "'," + amount + ")";
 		try(Connection cnn = DriverManager.getConnection(cnnStr); PreparedStatement statement = cnn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);){
 			ResultSet resultSet = null;
 			statement.execute();
@@ -575,7 +576,7 @@ public class ExampleQuery {
 
 	private boolean notInPantry(String user_ID, String ingre_name, String cnnStr)
 	{
-		String sql="SELECT Name FROM [Has]  WHERE User_Id="+ user_ID +"and Name='"+ ingre_name +"'";
+		String sql="SELECT Name FROM [Has]  WHERE User_Id="+ user_ID +"and Name='"+ ingre_name.toLowerCase() +"'";
 		ResultSet resultSet = null;
 		try(Connection cnn = DriverManager.getConnection(cnnStr); Statement statement = cnn.createStatement();){
 			resultSet = statement.executeQuery(sql);
@@ -590,8 +591,8 @@ public class ExampleQuery {
 
 	// finds the instructions of a recipe
 	public void FI(String r_name, String cnnStr){
-		System.out.println("Finding instructions for " + r_name);
-		String sql="Select instructions FROM [Recipe] WHERE Name='" + r_name +"'";
+		System.out.println("Finding instructions for " + r_name.toLowerCase());
+		String sql="Select instructions FROM [Recipe] WHERE Name='" + r_name.toLowerCase() +"'";
 		ResultSet ans=null;
 		try(Connection cnn = DriverManager.getConnection(cnnStr); Statement statement = cnn.createStatement();){
 			ans = statement.executeQuery(sql);
